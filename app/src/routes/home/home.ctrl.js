9b5a8b78b54b6866f8f031a649c6e8ebd6db5b25
HANSONGMU\hsm <hsm>
2022-01-23 11:54:53 +0900

    프런트 로그인 기능 구현

---
+++ app/src/routes/home/home.ctrl.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const output = {
+    home:  (req, res) => {
+    res.render("home/index");
+    },
+
+    login:  (req, res) => {
+    res.render("home/login");
+    },
+};
+
+const users = {
+    id: ["test", "한송무"],
+    psword: ["1234","1234"],
+};
+
+const process = {
+    login: (req, res) => {
+       const id = req.body.id,
+       psword = req.body.psword;
+
+       if (users.id.includes(id)){
+           const idx = users.id.indexOf(id);
+           if (users.psword[idx] === psword){
+
+               return res.json({
+                   success: true,
+               });
+           }
+
+       return res.json({
+           success: false,
+           msg: "로그인에 실패하셨습니다.",
+       });
+      }
+    }
+}
+
+module.exports = {
+    output,
+    process,
+};
