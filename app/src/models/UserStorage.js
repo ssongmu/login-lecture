e7444594c0b778e23b1b1ce689f753a1a0540738
HANSONGMU\hsm <hsm>
2022-02-06 10:37:48 +0900

    fs 모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,41 +1,48 @@
 "use strict";
 
+const fs = require("fs").promises;
+
 class userStorage {
-    static #users = {
-        id: ["test", "한송무", "test2"],
-        psword: ["1234","1234", "1234"],
-        name: ["테스트", "에물", "쏭무"],
-    };
 
-    static getUsers(...fields) {
-        const users = this.#users;
-        const newUsers = fields.reduce((newUsers, field) => {
-            if (users.hasOwnProperty(field)) {
-                newUsers[field] = users[field];
-            }
-            return newUsers;
-        }, {});;
-        return newUsers; 
-    }
-
-    static getUserInfo(id) {
-        const users = this.#users; 
+    static #getUserInfo(data, id) {
+        const users = JSON.parse(data);
         const idx = users.id.indexOf(id);
         const usersKeys = Object.keys(users);
         const userInfo = usersKeys.reduce((newUser, info) => {
             newUser[info] = users[info][idx];
             return newUser;
-        }, {});
-
+        }, {});     
         return userInfo;
     }
+    
+    static getUsers(...fields) {
+        //const users = this.#users;
+        // const newUsers = fields.reduce((newUsers, field) => {
+        //     if (users.hasOwnProperty(field)) {
+        //         newUsers[field] = users[field];
+        //     }
+        //     return newUsers;
+        // }, {});;
+        // return newUsers; 
+    }
+
+    static getUserInfo(id) {
+        return fs
+          .readFile("./src/databases/users.json")
+          .then((data) => {
+            return this.#getUserInfo(data, id);
+          })
+          .catch(console.error);
+    }
+
+  
 
     static save(userInfo) {
-        const users = this.#users;
-        users.id.push(userInfo.id);
-        users.name.push(userInfo.name);
-        users.psword.push(userInfo.psword);
-        return { success : true };
+       // const users = this.#users;
+        // users.id.push(userInfo.id);
+        // users.name.push(userInfo.name);
+        // users.psword.push(userInfo.psword);
+        // return { success : true };
     }
 }
 
